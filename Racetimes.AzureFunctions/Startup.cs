7382b9a4be1e83a67eb019940f351ec8196a90c3
Craig Nicol <craig.nicol@screenmedia.co.uk>
2020-02-05 13:19:21 +0000

    Add EventFlow to Startup

---
+++ Racetimes.AzureFunctions/Startup.cs
@@ -0,0 +1,58 @@
+﻿using EventFlow;
+using EventFlow.Configuration;
+using EventFlow.Extensions;
+using EventFlow.MsSql;
+using EventFlow.MsSql.EventStores;
+using EventFlow.MsSql.Extensions;
+using EventFlow.Snapshots.Strategies;
+using log4net;
+using log4net.Config;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Racetimes.Domain.Aggregate;
+using Racetimes.Domain.Command;
+using Racetimes.Domain.CommandHandler;
+using Racetimes.Domain.Event;
+using Racetimes.ReadModel.EntityFramework;
+using Racetimes.ReadModel.MsSql;
+using System.IO;
+using System.Reflection;
+
+[assembly: FunctionsStartup(typeof(Racetimes.AzureFunctions.Startup))]
+
+namespace Racetimes.AzureFunctions
+{
+    public class Startup : FunctionsStartup
+    {
+        private EventFlow.Configuration.IRootResolver CreateEventFlowResolver()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            // Let Singleton Scope in Configure() handle IDisposable
+            var resolver = EventFlowOptions.New
+                .UseConsoleLog()
+                .AddEvents(Assembly.GetAssembly(typeof(CompetitionRegisteredEvent)))
+                .AddCommands(Assembly.GetAssembly(typeof(RegisterCompetitionCommand)), t => true)
+                .AddCommandHandlers(Assembly.GetAssembly(typeof(RegisterCompetitionHandler)))
+                .AddSnapshots(Assembly.GetAssembly(typeof(CompetitionSnapshot)), t => true)
+                .RegisterServices(sr => sr.Register(i => SnapshotEveryFewVersionsStrategy.Default))
+                .UseMssqlEventStore()
+                .UseMsSqlSnapshotStore()
+                .AddMsSqlReadModel()
+                .AddEntityFrameworkReadModel()
+                .CreateResolver();
+            
+            // TODO: Get migration working without SqlClient configuration errors
+            // var msSqlDatabaseMigrator = resolver.Resolve<IMsSqlDatabaseMigrator>();
+            // EventFlowEventStoresMsSql.MigrateDatabase(msSqlDatabaseMigrator);
+
+            return resolver;
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton(typeof(IRootResolver), CreateEventFlowResolver());
+        }
+    }
+}
