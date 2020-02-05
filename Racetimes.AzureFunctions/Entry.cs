96ec0f0a358005321c8e2f17ff115c7e78ffdcf9
Craig Nicol <craig.nicol@screenmedia.co.uk>
2020-02-05 11:03:44 +0000

    Split Entry operations into their own functions

--- Racetimes.AzureFunctions/Entry.cs
+++ Racetimes.AzureFunctions/Entry.cs
@@ -12,13 +12,37 @@ namespace Racetimes.AzureFunctions
 {
     public static class Entry
     {
-        // Supports:
-        // POST - Record
-        // PUT - Corrent Data
-        // GET - Retrieve
-        [FunctionName("Entry")]
+        [FunctionName("GetEntries")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", "put", Route = null)] HttpRequest req,
+           [HttpTrigger(AuthorizationLevel.Function, "get", Route = "entry")] HttpRequest req,
+           ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+
+        [FunctionName("GetEntry")]
+        public static async Task<IActionResult> Get(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "entry/{id}")] HttpRequest req,
+            ILogger log, string id)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new NotFoundObjectResult($"Entry {id} does not exist");
+        }
+
+        [FunctionName("PostEntry")]
+        public static async Task<IActionResult> Post(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "entry")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -32,6 +56,17 @@ namespace Racetimes.AzureFunctions
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+
+        [FunctionName("PutEntry")]
+        public static async Task<IActionResult> Put(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "entry/{id}")] HttpRequest req,
+            ILogger log, string id)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            return new NotFoundObjectResult($"Entry {id} does not exist");
         }
     }
 }
